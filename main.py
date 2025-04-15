c3cdebde9592b29e83abc669eed1b707aa5180b6
lleach82 <lance.r.leach@gmail.com>
2025-04-15 07:59:32 -0400

    adjustments and added files

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    pygame.init
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x=SCREEN_WIDTH / 2,y=SCREEN_HEIGHT / 2)
     shutdown = False
 
     while shutdown != True:
@@ -16,7 +20,10 @@ def main():
                 return
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
